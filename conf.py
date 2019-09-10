51d88528f6623a2152307a1296aa059094c6b837
Zhiyu <huangzy2k@gmail.com>
2019-09-10 23:18:56 +0800

    conf

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'Docs Hub'
 copyright = '2019, ISCAS'
-author = 'DS&AI Lab.'
+author = 'DSI Lab'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
