d81d2c1280b11d45d7b1abf21afc720a0c1ddb27
Zhiyu <huangzy2k@gmail.com>
2019-09-14 01:04:49 +0800

    conf

--- conf.py
+++ conf.py
@@ -109,7 +109,7 @@ html_theme = 'sphinx_rtd_theme'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 html_logo = "DSAI Lab.svg"
-html_favicon = "DSAI logo.png"
+html_favicon = "DS and AI cat.png"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
